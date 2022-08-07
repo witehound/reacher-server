f219542087da0edba82ca1471e1fc1239c37e85f
InventedMost <netvid00@gmail.com>
2022-08-07 17:03:26 +0100

    cleaned up the goalroutes file

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -5,26 +5,9 @@ import {
   getGoals,
   deleteGoal,
 } from "../controllers/goalControllers.js";
-const route = Router();
+const router = Router();
 
-//@desc Set Goal
-//@route GET /api/goal
-//@access Private
-route.get("/", getGoals);
+router.route("/").get(getGoals).post(postGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
 
-//@desc Post Goal
-//@route POST /api/goal
-//@access Private
-route.post("/", postGoal);
-
-//@desc Update Goal
-//@route PUT /api/goal/:id
-//@access Private
-route.put("/:id", updateGoal);
-
-//@desc Delete Goal
-//@route DELETE /api/goal
-//@access Private
-route.delete("/:id", deleteGoal);
-
-export default route;
+export default router;
