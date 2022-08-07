43446c3f6177108dc1f8c452afc73e17e3e4591e
InventedMost <netvid00@gmail.com>
2022-08-07 16:57:16 +0100

    added comment in goalroutes

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -7,12 +7,24 @@ import {
 } from "../controllers/goalControllers.js";
 const route = Router();
 
+//@desc Set Goal
+//@route GET /api/goal
+//@access Private
 route.get("/", getGoals);
 
+//@desc Post Goal
+//@route POST /api/goal
+//@access Private
 route.post("/", postGoal);
 
+//@desc Update Goal
+//@route PUT /api/goal/:id
+//@access Private
 route.put("/:id", updateGoal);
 
+//@desc Delete Goal
+//@route DELETE /api/goal
+//@access Private
 route.delete("/:id", deleteGoal);
 
 export default route;
